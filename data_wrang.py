0080b6389b807e35c30b9c3a0f7ada27db78eb1d
derecksdev <73000078+derecksdev@users.noreply.github.com>
2021-03-10 13:36:24 -0500

    Functions for piechart creation

---
+++ data_wrang.py
@@ -0,0 +1,45 @@
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import csv
+
+
+def convert_to_perc(perc):
+    total = 0.0
+    for i in perc:
+        total += i
+    for i in range(len(perc)):
+        perc[i] = perc[i] / total
+
+
+def make_piechart(arr):
+    convert_to_perc(arr)
+
+    labels = 'Negative', 'Neutral', 'Positive'
+    fig1, ax1 = plt.subplots()
+    ax1.pie(arr, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.show()
+
+
+def get_pol_sub(value, perc):
+    if value > 0:
+        perc[2] += 1
+    elif value == 0:
+        perc[1] += 1
+    else:
+        perc[0] += 1
+
+# This is an updated implementation for the function in 'sent_ana.py'
+def get_text_sub(data):
+    i = TextBlob(data).sentiment.subjectivity
+    get_pol_sub(i, sub_arr)
+    return i
+
+# This is an updated implementation for the function in 'sent_ana.py'
+def get_text_pol(data):
+    i = TextBlob(data).sentiment.polarity
+    get_pol_sub(i, pol_arr)
+    return i
